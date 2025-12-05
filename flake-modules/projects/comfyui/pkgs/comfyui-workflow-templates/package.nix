bb3ef83f5b1f8727704bcaf88451197e65b9c22d
lodjuret <albin.aberg@proton.me>
2025-12-05 18:03:52 +0100

    Combined comfyui-workflow-templates with core

--- flake-modules/projects/comfyui/pkgs/comfyui-workflow-templates/package.nix
+++ flake-modules/projects/comfyui/pkgs/comfyui-workflow-templates/package.nix
@@ -2,38 +2,65 @@
   python3Packages,
   comfyuiNpins,
 }: let
-  npin = comfyuiNpins.comfyui-workflow-templates;
-in
-  python3Packages.callPackage (
+  npinMain = comfyuiNpins.comfyui-workflow-templates;
+  npinCore = comfyuiNpins.comfyui-workflow-templates-core;
+
+  core = python3Packages.callPackage (
     {
-      lib,
       buildPythonPackage,
       fetchurl,
+      lib,
     }:
       buildPythonPackage rec {
-        pname = "comfyui_workflow_templates";
-
-        inherit (npin) version;
+        pname = "comfyui_workflow_templates_core";
+        inherit (npinCore) version;
 
         src = fetchurl {
-          inherit (npin) url;
-          sha256 = npin.hash;
+          inherit (npinCore) url;
+          sha256 = npinCore.hash;
         };
 
         format = "setuptools";
 
-        propagatedBuildInputs = [
-          python3Packages.comfyui_workflow_templates_core
-        ];
-
-        pythonImportsCheck = [
-          pname
-        ];
+        pythonImportsCheck = [pname];
 
         meta = with lib; {
-          description = "ComfyUI workflow templates available in the app by clicking the Workflow button then the Browse Templates button.";
+          description = "Core module for ComfyUI workflow templates.";
           homepage = "https://github.com/Comfy-Org/workflow_templates";
           license = licenses.gpl3;
         };
       }
-  ) {}
+  ) {};
+
+  main = python3Packages.callPackage (
+    {
+      buildPythonPackage,
+      fetchurl,
+      lib,
+    }:
+      buildPythonPackage rec {
+        pname = "comfyui_workflow_templates";
+        inherit (npinMain) version;
+
+        src = fetchurl {
+          inherit (npinMain) url;
+          sha256 = npinMain.hash;
+        };
+
+        format = "setuptools";
+
+        propagatedBuildInputs = [core];
+
+        pythonImportsCheck = [pname];
+
+        meta = with lib; {
+          description = "ComfyUI workflow templates available via the Browse Templates feature.";
+          homepage = "https://github.com/Comfy-Org/workflow_templates";
+          license = licenses.gpl3;
+        };
+      }
+  ) {};
+in {
+  comfyui_workflow_templates = main;
+  comfyui_workflow_templates_core = core;
+}
